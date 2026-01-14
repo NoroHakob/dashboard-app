3ee33c8fd029063c746f35a77bda046fc64d3c7f
NoroHakob <norohakob2004@gmail.com>
2026-01-14 16:37:56 +0400

    Dashboard-App(Dynamic Routes)

---
+++ app/blog/[blogId]/page.tsx
@@ -0,0 +1,13 @@
+interface BlogIdPageProps {
+    params: Promise<{ blogId: string }>  
+}
+
+export default async function BlogIdPage({ params }: BlogIdPageProps) {
+    const {blogId} = await params 
+    return (
+        <div>
+            <h1>Hello from the blog id page</h1>
+            <p>blogId: {blogId}</p>
+        </div>
+    )
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function BlogPage() {
+    return (
+        <div>
+            <h1>Hello from the blog id page</h1>
+        </div>
+    )
+}
