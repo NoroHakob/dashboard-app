4a9814f3fdacf9c0c36203df3823f865a2d617e8
NoroHakob <norohakob2004@gmail.com>
2026-01-25 02:09:58 +0400

    Dashboard-App(+Blog Creation Route)

---
+++ app/(shared-layout)/create/page.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { postSchema } from "@/app/schemas/blog";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Field, FieldError, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import z from "zod";
+
+export default function CreateRoute() {
+    const form = useForm<z.infer<typeof postSchema>>({
+        resolver: zodResolver(postSchema),
+        defaultValues: {
+          content: "",
+          title: ""
+        },
+      });
+    return (
+        <div className="py-12">
+            <div className="text-center mb-12">
+                <h1 className="text-4xl font-extrabold tracking-tight sm:text-5xl">Create Post</h1>
+                <p className="text-xl text-muted-foreground pt-4">Create your own blog article...</p>
+            </div>
+
+            <Card className="w-full max-w-xl mx-auto">
+                <CardHeader>
+                    <CardTitle>Create Blog Article</CardTitle>
+                    <CardDescription>Create a new blog article</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form>
+                        <FieldGroup className="gap-y-4">
+                            <Controller
+                                name="title"
+                                control={form.control}
+                                render={({ field, fieldState }) => (
+                                    <Field>
+                                    <FieldLabel className="capitalize">Title</FieldLabel>
+                                    <Input
+                                        type="text"
+                                        aria-invalid={fieldState.invalid}
+                                        placeholder="super cool title"
+                                        {...field} 
+                                    />
+                                    {fieldState.error && <FieldError errors={[fieldState.error]} />}
+                                    </Field>
+                                )}
+                            />
+
+                            <Controller
+                                name="content"
+                                control={form.control}
+                                render={({ field, fieldState }) => (
+                                    <Field>
+                                    <FieldLabel className="capitalize">Content</FieldLabel>
+                                    <Textarea
+                                        aria-invalid={fieldState.invalid}
+                                        placeholder="Super cool blog content"
+                                        {...field} 
+                                    />
+                                    {fieldState.error && <FieldError errors={[fieldState.error]} />}
+                                    </Field>
+                                )}
+                            />
+                            <Button>Create Post</Button>
+                        </FieldGroup>
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
