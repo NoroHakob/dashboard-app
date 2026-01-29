53d743ea854ca7e3e144e40c007f22e09bcfe7b1
NoroHakob <norohakob2004@gmail.com>
2026-01-29 19:33:50 +0400

    Dashboard-App(+Image Optimization)

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -1,8 +1,11 @@
 "use client"
 
-import { Card } from "@/components/ui/card"
+import { buttonVariants } from "@/components/ui/button"
+import { Card, CardContent, CardFooter } from "@/components/ui/card"
 import { api } from "@/convex/_generated/api"
 import { useQuery } from "convex/react"
+import Image from "next/image"
+import Link from "next/link"
 
 export default function BlogPage() {
     const data = useQuery(api.posts.getPosts)
@@ -18,8 +21,29 @@ export default function BlogPage() {
 
         <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
             {data?.map((post) => (
-                <Card className="h-19" key={post._id}>
-                    {post.title}
+                <Card className="pt-0 text-center" key={post._id}>
+                    <div className="relative h-48 w-full overflow-hidden">
+                        <Image
+                            src="https://images.unsplash.com/photo-1524504388940-b1c1722653e1"
+                            alt="Blog image"
+                            fill
+                            className="rounded-t-lg"
+                        />
+                    </div>
+
+                    <CardContent>
+                        <Link href={`/blog/${post._id}`}>
+                        <h1 className="text-2xl font-bold hover:text-primary">{post.title}</h1>    
+                        </Link>
+                        <p className="text-muted-foreground line-clamp-3">{post.body}</p>
+                    </CardContent>
+                    <CardFooter>
+                        <Link className={buttonVariants({   
+                            className: "w-full" 
+                        })}  href={`/blog/${post._id}`}>
+                            Read more
+                        </Link>
+                    </CardFooter>
                 </Card>
 
             ))}
